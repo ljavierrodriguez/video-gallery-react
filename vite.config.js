8fdf48dcaf72dae69c3526e1e86a62b7911ff958
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2024-02-02 10:22:09 -0300

    Fixing Load .env

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  },
+export default defineConfig((command, mode) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react()],
+    define: {
+      'process.env': env
+    }
+  }
 })
